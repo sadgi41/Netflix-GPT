baaa24f92ea8c77488ce2980e3e7db14cae4287f
Sadgi Jain <sadgijain41@gmail.com>
2026-01-21 20:59:25 +0530

    final changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -67,19 +67,19 @@ const Login = () => {
             </div>
             <div className='flex justify-center '>
                 <form onSubmit={(e) => e.preventDefault()}
-                    className='absolute bg-black bg-opacity-75 w-auto md:w-3/12 p-12  my-36 mx-auto left-0 right-0 text-white rounded-lg'>
+                    className='absolute bg-black bg-opacity-75 w-1/2 md:w-4/12 p-6 my-36 mx-auto left-0 right-0 text-white rounded-lg'>
 
-                    <h1 className='text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+                    <h1 className='text-xl md:text-2xl py-2'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
 
                     {!isSignInForm && <input ref={name} type='text' placeholder='Full Name' className='p-2 my-2  w-full bg-slate-600' />}
 
-                    <input ref={email} type='text' placeholder='Email' className='p-2 my-2  w-full bg-slate-600' />
+                    <input ref={email} type='text' placeholder='Email' className='p-2 my-1  w-full bg-slate-600' />
 
-                    <input ref={password} type='password' placeholder='Password' className='p-2  my-2 bg-slate-600  w-full' />
+                    <input ref={password} type='password' placeholder='Password' className='p-2 my-1 bg-slate-600  w-full' />
 
                     <p className='text-red-600 text-lg'>{errorMessage}</p>
 
-                    <button className='p-2  my-4   bg-red-700 rounded-lg w-full' onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
+                    <button className='p-2 my-2  bg-red-700 rounded-lg w-full' onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
 
                     <p className='my-2 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign Up Now." : "Already Registered. Sign In Then."}</p>
                 </form>
