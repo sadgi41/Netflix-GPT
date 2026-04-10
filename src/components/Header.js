3190183cff9468f0436ba54e5014c73754be469c
Sadgi Jain <sadgijain41@gmail.com>
2026-04-10 14:51:08 +0530

    Remove unused import of React in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react';
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from 'react-router-dom';
